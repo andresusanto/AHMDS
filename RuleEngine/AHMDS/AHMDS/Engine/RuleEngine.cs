4b1a4632e983d2659061383fd12aa05a84b60370
Andre Susanto <andre@andresusanto.info>
2016-03-23 16:04:06 +0700

    Integrating project with SWI-Prolog

--- RuleEngine/AHMDS/AHMDS/Engine/RuleEngine.cs
+++ RuleEngine/AHMDS/AHMDS/Engine/RuleEngine.cs
@@ -2,11 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SbsSW.SwiPlCs;
 
 namespace AHMDS.Engine
 {
     class RuleEngine
     {
+        private const string SWI_HOME_DIR = @"C:\Program Files\swipl";
 
+        public RuleEngine()
+        {
+            String[] param = { "-q" };  // suppressing informational and banner messages
+            //Environment.SetEnvironmentVariable("SWI_HOME_DIR", SWI_HOME_DIR);
+            PlEngine.Initialize(param);
+        }
+
+        public void tes()
+        {
+            
+            PlQuery.PlCall("assert(father(martin, inka))");
+            PlQuery.PlCall("assert(father(uwe, gloria))");
+            PlQuery.PlCall("assert(father(uwe, melanie))");
+            PlQuery.PlCall("assert(father(uwe, ayala))");
+            using (var q = new PlQuery("father(P, C), atomic_list_concat([P,' is_father_of ',C], L)"))
+            {
+                foreach (PlQueryVariables v in q.SolutionVariables)
+                    Console.WriteLine(v["L"].ToString());
+
+                Console.WriteLine("all children from uwe:");
+                q.Variables["P"].Unify("uwe");
+                foreach (PlQueryVariables v in q.SolutionVariables)
+                    Console.WriteLine(v["C"].ToString());
+            }
+            PlEngine.PlCleanup();
+            Console.WriteLine("finshed!");
+        }
     }
 }
--- RuleEngine/AHMDS/AHMDS/Form1.cs
+++ RuleEngine/AHMDS/AHMDS/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using AHMDS.Engine;
 
 namespace AHMDS
 {
@@ -14,6 +15,12 @@ namespace AHMDS
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RuleEngine rule = new RuleEngine();
+            rule.tes();
         }
     }
 }
