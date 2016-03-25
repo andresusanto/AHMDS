7d63b0e897e2d379ffbdcd64d8d2970396b4dd6f
Andre Susanto <andre@andresusanto.info>
2016-03-25 14:48:37 +0700

    add explanation to rule

--- RuleEngine/Rules/APICallRules.pl
+++ RuleEngine/Rules/APICallRules.pl
@@ -1,22 +1,26 @@
 :- dynamic explanation/1.
 explanation([]).
 
+tambah_explanation(New) :- explanation(L1), append(L1, New, L2), retractall(explanation(L1)), asserta(explanation(L2)).
+hapus_explanation :- retractall(explanation(_)), asserta(explanation([])).
+
+
 % score individual
-	mi_score(120, "ANDRE").
-	mi_score(20, "SUSANTO").
-	mi_score(20, "METAL").
+	mi_score(120, "ANDRE") :- tambah_explanation(["Program mengakses reg"]).
+	mi_score(20, "SUSANTO") :- tambah_explanation(["Program mengakses file system"]).
+	mi_score(20, "METAL") :- tambah_explanation(["Program mengakses metal"]).
 	mi_score(0, _).
 	
 	i_score(X, Y) :- X = [H|T], i_score(T, Z), mi_score(V, H),!, Y is V+Z.
-	i_score([], Y) :- Y is 0.
+	i_score([], 0).
 	
 % score keterhubungan (suatu kombinasi API akan menghasilkan skor lebih tinggi dibanding API tersebut jalan secara terpisah2)
-	ml_score(10, ["ANDRE", "METAL"]).
-	ml_score(10, ["ANDRE", "SUSANTO"]).
+	ml_score(10, ["ANDRE", "METAL"]) :- tambah_explanation(["Program diduga mengirim data rekaman ke internet"]).
+	ml_score(10, ["ANDRE", "SUSANTO"]) :- tambah_explanation(["Program diduga memanfaatkan fitur ABC"]).
 	
 	al_score(X, Y) :- ml_score(Y, L), subset(L, X).
-	l_score(X, Y) :- aggregate(sum(C), al_score(X, C), Y). 
-	
+	l_score(X, Y) :- aggregate(sum(C), al_score(X, C), Y), !. 
+	l_score(_, 0).
 
 % Fungsi untuk menghitung score total
-	score(X, Y) :- i_score(X, Z), Y is Z.
+	score(X, Y) :- hapus_explanation, i_score(X, Z), l_score(X, V), Y is Z+V.
